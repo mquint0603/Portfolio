f9a7be5c8b9f8b371ae3aaa779c1709bb531f91f
Mary Quint <mary.k.quint@gmail.com>
2018-06-13 17:07:45 -0500

    homepage

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+$("i").mouseenter(function() {
+    $(this).animate({opacity: .5});
+});
+
+$("i").mouseleave(function() {
+    $(this).animate({opacity: 1.0});
+});
+
+$(".proj-image").mouseenter(function() {
+    $(this).animate({opacity: .5});
+});
+
+$(".proj-image").mouseleave(function() {
+    $(this).animate({opacity: 1.0});
+});
+
